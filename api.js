3fb053f30d1884261e160d87502409df2c38a06d
Erick Basilio Olegario <ra49236583806@cursos.insf>
2024-08-02 11:24:11 -0300

    preojeto

--- api.js
+++ api.js
@@ -1,4 +1,20 @@
 import express from "express";
 const servidor = express();
 
+servidor.use(express.json());
 
+servidor.get('/somadora/:n1/:n2', (req, resp)=>{
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let soma = n1+n2;
+
+    resp.send('a soma é '+  soma);
+})
+
+
+
+servidor.listen(
+    5050,
+    () => console.log('API BRABA')
+)
